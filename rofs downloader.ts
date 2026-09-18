ed9ce9e3f97a48b2b030fd5ce8ef2e4710697641
Windows10Fan <coolc4228@gmail.com>
2026-09-18 05:41:05 -0700

    Modernize ROFS downloader types and async code

--- rofs downloader.ts
+++ rofs downloader.ts
@@ -8,11 +8,11 @@ export async function downloadRofsFile(outDir = __dirname, w96Domain = "https://
     if (fs.existsSync(rofsPath)) fs.rmSync(rofsPath);
 
     const url = `${w96Domain}${rofsLocation}`;
-    return await downloadFile(url, outDir);
+    return downloadFile(url, outDir);
 }
 
 export async function downloadFile(file: string, outDir = __dirname): Promise<boolean> {
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve) => {
         const downloader = new DownloaderHelper(file, outDir);
 
         downloader.on("end", () => {
@@ -20,12 +20,12 @@ export async function downloadFile(file: string, outDir = __dirname): Promise<bo
             resolve(true);
         });
 
-        downloader.on("error", err => {
+        downloader.on("error", (err: unknown) => {
             console.error(`Download of file ${file} failed`, err);
             resolve(false);
         });
-    
-        downloader.start().catch( err => {
+
+        downloader.start().catch((err: unknown) => {
             console.error(`Download of file ${file} failed`, err);
             resolve(false);
         });
@@ -33,15 +33,15 @@ export async function downloadFile(file: string, outDir = __dirname): Promise<bo
 }
 
 export async function readRofs(rofsFile = "./rofs.json"): Promise<RofsJson> {
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         let rofsString = "";
         const rofsReadStream = fs.createReadStream(rofsFile);
         rofsReadStream.setEncoding("utf8");
-        
-        rofsReadStream.on("data", data => rofsString += data);
+
+        rofsReadStream.on("data", (data: string) => rofsString += data);
         rofsReadStream.on("end", () => {
-            const rofs = JSON.parse(rofsString);
-            
+            const rofs = JSON.parse(rofsString) as RofsJson;
+
             rofs["/system/images"] = { length: 0, type: fileType.directory };
             rofs["/system/images/rootfs"] = { length: 0, type: fileType.directory };
 
@@ -56,22 +56,22 @@ export async function readRofs(rofsFile = "./rofs.json"): Promise<RofsJson> {
                 "rootfs/recovery.zip",
                 "rootfs/oobe.zip",
                 "rootfs/rootfs.zip"
-            ].forEach( file => {
+            ].forEach((file: string) => {
                 rofs[`/system/images/${file}`] = { length: 0, type: fileType.file };
             });
 
             rofs["/vc"] = { length: 0, type: fileType.directory };
             rofs["/vc/ct.js"] = { length: 0, type: fileType.file };
-            
+
             resolve(rofs);
         });
-        rofsReadStream.on("error", err => reject(err) );
+        rofsReadStream.on("error", reject);
     });
 }
 
 export async function createDirectories(rofs: RofsJson, outDir = __dirname): Promise<void> {
     for (const fileOrDir in rofs) {
-        if (rofs[fileOrDir].type == fileType.directory) {
+        if (rofs[fileOrDir].type === fileType.directory) {
             const dir = path.join(outDir, fileOrDir);
             if (!fs.existsSync(dir) || !fs.statSync(dir).isDirectory()) fs.mkdirSync(dir);
         }
@@ -80,7 +80,7 @@ export async function createDirectories(rofs: RofsJson, outDir = __dirname): Pro
 
 export async function downloadFiles(rofs: RofsJson, outDir = __dirname, w96Domain = "https://windows96.net"): Promise<void> {
     for (const fileOrDir in rofs) {
-        if (rofs[fileOrDir].type == fileType.file) {
+        if (rofs[fileOrDir].type === fileType.file) {
             const url = `${w96Domain}${fileOrDir}`;
             const fileDir = path.join(outDir, path.dirname(fileOrDir));
             await downloadFile(url, fileDir);
@@ -99,7 +99,7 @@ export default async function iniateDownload(outDir = __dirname, w96Domain = "ht
     if (!await downloadRofsFile(outDir, w96Domain)) throw new Error("Failed to download rofs.json");
     console.log("rofs.json downloaded successfully");
 
-    return await downloadRofsContents(outDir, w96Domain);
+    await downloadRofsContents(outDir, w96Domain);
 }
 
 if (require.main === module) iniateDownload(__dirname, process.argv[2]).then(() => console.log("Done"));
